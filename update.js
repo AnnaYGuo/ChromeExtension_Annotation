af1857a2c140bde32aefede9eaeb7a9138ba065c
njayanthi21 <72714765+njayanthi21@users.noreply.github.com>
2022-02-27 10:38:04 -0600

    Update update.js

--- update.js
+++ update.js
@@ -1,11 +1,16 @@
 document.getElementById("refresh").onclick = async () => {
-        //document.body.append("refresh attempted" + document.createElement('br'));
+    //document.body.append("refresh attempted" + document.createElement('br'));
     //savedtext.append("refresh attempted");
-    document.body.append(document.createElement('br'));
-    chrome.storage.local.get(['key'], function(result) {
+        document.body.append(document.createElement('br'));
+        chrome.storage.local.get(['key'], function(result) {
         for(var ii = 0; ii < result.key.length; ii++){
-            savedtext.append(result.key[ii]);
-            savedtext.append(document.createElement('br'));
+            document.body.append('New highlight:    "');
+            document.body.append(result.key[ii]);
+            document.body.append('"');
+            //alert('ooooooooooo');
         }
+        
+        document.body.append(document.createElement('br'));
      });
+    
 }
