fbbad11d3a9c351d9565def87516b661a6013a95
AnnaYGuo <91708417+AnnaYGuo@users.noreply.github.com>
2022-02-27 07:43:08 -0600

    Update update.js
    Clean, "prints" all stored array values

--- update.js
+++ update.js
@@ -1,8 +1,11 @@
 document.getElementById("refresh").onclick = async () => {
         //document.body.append("refresh attempted" + document.createElement('br'));
-    document.body.append("refresh attempted");
+    //savedtext.append("refresh attempted");
     document.body.append(document.createElement('br'));
     chrome.storage.local.get(['key'], function(result) {
-        document.body.append(result.key);
+        for(var ii = 0; ii < result.key.length; ii++){
+            savedtext.append(result.key[ii]);
+            savedtext.append(document.createElement('br'));
+        }
      });
 }
