df69f6260770ecf8b41898187d395bf33c4a8ce6
AnnaYGuo <91708417+AnnaYGuo@users.noreply.github.com>
2022-02-26 23:14:31 -0600

    Update update.js
    Successfully prints locally stored "result"; "result" is a singular stored string, next goal is to print from stored arrays

--- update.js
+++ update.js
@@ -1,5 +1,8 @@
-var saveID = [];
-
-for(int ii = 0; ii<ID.length; ii++){
-	chrome.storage.sync.get(var[ii]: function(data))
+document.getElementById("refresh").onclick = async () => {
+        //document.body.append("refresh attempted" + document.createElement('br'));
+    document.body.append("refresh attempted");
+    document.body.append(document.createElement('br'));
+    chrome.storage.local.get(['key'], function(result) {
+        document.body.append(result.key);
+     });
 }
